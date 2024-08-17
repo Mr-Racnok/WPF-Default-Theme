21e76a1cb1eb2e21b0773ad6e144e9b63ecb7c77
Mr-Racnok <jougocheater@gmail.com>
2024-08-17 22:02:51 +0700

    Update MainWindow.xaml.vb

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,4 +1,18 @@
+Imports System.Media
+Imports Template2.Template2
+Imports Wpf.Ui.Controls
+
 Class MainWindow
+
+    Public Sub New()
+        InitializeComponent()
+        With SideNavigation
+            .IsPaneOpen = False
+            .Visibility = Visibility.Collapsed
+        End With
+        goto_Login()
+    End Sub
+
     Private Sub Border_MouseDown(sender As Object, e As MouseButtonEventArgs)
         If e.LeftButton = MouseButtonState.Pressed Then
             Me.DragMove()
@@ -13,6 +27,21 @@ Class MainWindow
                 Me.WindowState = WindowState.Maximized
             End If
         End If
+    End Sub
+
+    Sub goto_Login()
+        Content.Navigate(New Uri("Login.xaml", UriKind.Relative))
+    End Sub
+
+    Private Sub OpenDialog_Click(sender As Object, e As RoutedEventArgs)
+        Dim dialog As New DialogWindowExit()
+        With dialog
+            .DialogTittle.Text = "Warning"
+            .DialogText.Text = "Seluruh proses yang belum disimpan akan hilang jika anda keluar."
+            .DialogButtonYes.Appearance = ControlAppearance.Danger
+            .DialogButtonYes.Foreground = New BrushConverter().ConvertFromString("#ffffff")
+        End With
+        dialog.ShowDialog()
     End Sub
 
 End Class
